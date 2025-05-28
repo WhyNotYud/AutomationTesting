4bbef4c0282d47fad4c71923ad22b9edd8b52d6a
WhyNotYud <mtd.contactwithme@gmail.com>
2025-05-29 02:10:23 +0700

    feat: implement base test setup and teardown

---
+++ src/main/java/com/somee/base/BaseTest.java
@@ -0,0 +1,27 @@
+package com.somee.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    public WebDriver driver;
+    protected static final String BASE_URL = "http://hauiproj.somee.com/Default.aspx";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}

1f009cb7928347b64459d064c7ee190e20d16875
WhyNotYud <mtd.contactwithme@gmail.com>
2025-05-29 05:41:31 +0700

    feat: add common web interaction and validation utilities

---
+++ src/main/java/com/somee/utils/ValidateUtils.java
@@ -0,0 +1,56 @@
+package com.somee.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class ValidateUtils {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private final int timeoutWaitForPageLoaded = 10;
+
+    public ValidateUtils(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public void setText(By element, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        driver.findElement(element).clear();
+        driver.findElement(element).sendKeys(text);
+    }
+
+    public String getText(By element) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+        return driver.findElement(element).getText();
+    }
+
+    public void clickElement(By element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        driver.findElement(element).click();
+    }
+
+    public void waitForPageLoaded() {
+        ExpectedCondition<Boolean> expectedConditions = new ExpectedCondition<>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutWaitForPageLoaded));
+            wait.until(expectedConditions);
+        } catch (Throwable e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    public boolean verifyUrl(String url) {
+        return driver.getCurrentUrl().contains(url);
+    }
+}

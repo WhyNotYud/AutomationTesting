5c68e686d7ec17196196f852a2aede2f976a0940
WhyNotYud <mtd.contactwithme@gmail.com>
2025-06-01 08:53:14 +0700

    update: add order function

--- src/main/java/com/somee/pages/OrderPage.java
+++ src/main/java/com/somee/pages/OrderPage.java
@@ -1,6 +1,7 @@
 package com.somee.pages;
 
 import com.somee.utils.ValidateUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,11 +12,20 @@ public class OrderPage {
     private WebDriverWait wait;
     private ValidateUtils validateUtils;
 
+    private By thanhToanButton = By.id("ContentPlaceHolder1_btnThanhToan");
+    private By ghiChuInput = By.id("ContentPlaceHolder1_dtlThongTinUser_txtGhiChu_0");
+    private By datHangButton = By.id("ContentPlaceHolder1_dtlThongTinUser_btnThanhToan_0");
+
     public OrderPage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         validateUtils = new ValidateUtils(driver);
     }
 
-
+    public void Order(String ghiChu) {
+        validateUtils.waitForPageLoaded();
+        validateUtils.clickElementWithJS(thanhToanButton);
+        validateUtils.setValueWithJS(ghiChuInput, ghiChu);
+        validateUtils.clickElementWithJS(datHangButton);
+    }
 }

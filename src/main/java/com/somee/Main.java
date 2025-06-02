7769e491feabbd9b9366095904c2697e9ac6b942
WhyNotYud <mtd.contactwithme@gmail.com>
2025-06-02 16:12:56 +0700

    feat: add Main.java to run all test case

--- src/main/java/com/somee/Main.java
+++ src/main/java/com/somee/Main.java
@@ -1,17 +1,26 @@
 package com.somee;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.somee.tests.*;
+import org.testng.TestNG;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        TestNG testng = new TestNG();
+        List<Class<?>> classes = new ArrayList<>();
+        classes.add(RegisterTest.class);
+        classes.add(LoginTest.class);
+        classes.add(LogoutTest.class);
+        classes.add(CartTest.class);
+        classes.add(OrderTest.class);
+        classes.add(CategoryTest.class);
+        for(int i = 0; i < 6; i++) {
+            testng.setTestClasses(classes.toArray(new Class[i]));
         }
+        System.out.println("Bắt đầu chạy các TestNG tests.");
+        testng.run();
+        System.out.println("Các TestNG tests đã hoàn thành.");
     }
 }
